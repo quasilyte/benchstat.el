c7a2100988f9b15a28c9c22ee27d5bc74613f05f
quasilyte <quasilyte@gmail.com>
2017-10-08 14:53:34 +0300

    impl: profile-based API

--- benchstat.el
+++ benchstat.el
@@ -22,6 +22,19 @@
 
 ;;; Code:
 
+;; Skip to `Public:' section if you want to inspect package public API.
+(eval-when-compile
+  (defmacro benchstat--warn (msg &rest args)
+    `(display-warning 'benchstat
+                      (apply #'format-message (list ,msg ,@args))
+                      :warning))
+  ;; `profile' type is not meant to be used outside of this package,
+  ;; so define it as compile-time private macro.
+  (defmacro benchstat--make-profile (key filename) `(list ,key ,filename))
+  (defmacro benchstat--profile-key (profile) `(nth 0 ,profile))
+  (defmacro benchstat--profile-filename (profile) `(nth 1 ,profile))
+  )
+
 ;; Public:
 
 (defgroup benchstat nil
@@ -29,23 +42,11 @@
   :group 'lisp)
 
 (defcustom benchstat-program "benchstat"
-  "Command that is used to invoke `benchstat' utility."
+  "Command that is used to invoke `benchstat' program."
   :group 'benchstat)
 
 (defcustom benchstat-buffer "*benchstat*"
   "The name of temporary output buffer for benchstat compare functions."
-  :group 'benchstat)
-
-(defcustom benchstat-filename-new
-  (expand-file-name "benchstat-new"
-                    temporary-file-directory)
-  "File that is used to collect `new' code profile."
-  :group 'benchstat)
-
-(defcustom benchstat-filename-old
-  (expand-file-name "benchstat-old"
-                    temporary-file-directory)
-  "File that is used to collect `old' code profile."
   :group 'benchstat)
 
 (defvar benchstat-run-count 10
@@ -53,54 +54,85 @@
 The value of lower than 5 is unrecommended.
 The value higher than 10 can be good, but most of the time it's redundant.")
 
-(defun benchstat-compare (&optional new old delta-test)
-  "Compare profiles from NEW and OLD files by using the DELTA-TEST method.
+(defvar benchstat--profiles
+  (list (benchstat--make-profile
+         :old
+         (expand-file-name "benchstat-old" temporary-file-directory))
+        (benchstat--make-profile
+         :new
+         (expand-file-name "benchstat-new" temporary-file-directory)))
+  "Associative list of known profiles.")
+
+(defun benchstat-push-profile (key filename)
+  "Make profile with KEY point to FILENAME.
+The recommendation is to use keywords as profile key.
+Pushing same KEY will overwrite old value.
+
+FILENAME should be a full file name (path + base name).
+It can be a relative path (i.e. just \"file name\"), but
+do so only if you know what you are doing.
+
+Please note that non-temp files are not going to be removed automatically."
+  (unless (symbolp key)
+    (benchstat--warn "Profile KEY should be a symbol; preferably a keyword (got %S)"
+                     (type-of key)))
+  (unless (file-writable-p filename)
+    (error "FILENAME `%s' is not writeable" filename))
+  (push (benchstat--make-profile key filename)
+        benchstat--profiles))
+
+(defun benchstat-compare (&optional old new delta-test)
+  "Compare OLD and NEW profiles by using the DELTA-TEST method.
 By default:
-  - NEW set to `benchstat-filename-new'
-  - OLD set to `benchstat-filename-old'
-  - DELTA-TEST set to \"utest\" (two-sample Welch)
+  - OLD = `:old'
+  - NEW = `:new'
+  - DELTA-TEST = \"utest\" (Mann-Whitney U-test)
 
-The alternative DELTA-TEST is \"ttest\" (two-sample Welch).
-
-If you want to specify only DELTA-TEST, use:
-\(benchstat-compare nil nil \"ttest\"\)
-
-You can compare older profiles by using explicit NEW and OLD."
+The alternative DELTA-TEST is \"ttest\" (two-sample Welch)."
   (interactive)
-  (let ((new (or new benchstat-filename-new))
-        (old (or old benchstat-filename-old))
+  (let ((old (benchstat--profile (or old :old)))
+        (new (benchstat--profile (or new :new)))
         (delta-test (or delta-test "utest")))
     (with-output-to-temp-buffer benchstat-buffer
       (shell-command
-       (format "%s -delta-test=%s %s %s" benchstat-program delta-test new old)
+       (format "%s -delta-test=%s %s %s"
+               benchstat-program
+               delta-test
+               (benchstat--profile-filename old)
+               (benchstat--profile-filename new))
        benchstat-buffer))))
 
-(defun benchstat-reset (tag)
-  "Clear profile data for TAG."
-  (with-temp-file (benchstat--file tag)
+(defun benchstat-reset (key)
+  "Clear KEY profile data file."
+  (with-temp-file (benchstat--profile-filename (benchstat--profile key))
     (insert "")))
 
-(defmacro benchstat-run (TAG REPETITIONS &rest FORMS)
-  "Using TAG profile, run REPETITIONS timed evaluations of FORMS.
+(defmacro benchstat-run (KEY REPETITIONS &rest FORMS)
+  "Using KEY profile, run REPETITIONS timed evaluations of FORMS.
 Internally, uses `benchmark-run-compiled' to collect profile data.
 
 Overwrites profile data file.
 Use `benchstat-run-more' if you want to append instead."
   (declare (indent 2))
-  (let ((file (benchstat--file TAG)))
-    `(with-temp-file ,file
+  (let ((filename (benchstat--profile-filename (benchstat--profile KEY))))
+    `(with-temp-file ,filename
        (insert (benchstat--run ,REPETITIONS ,@FORMS)))))
 
-(defmacro benchstat-run-more (TAG REPETITIONS &rest FORMS)
+(defmacro benchstat-run-more (KEY REPETITIONS &rest FORMS)
   "Like `benchstat-run', but does not overwrite profile data file."
   (declare (indent 2))
-  (let ((file (benchstat--file TAG)))
+  (let ((filename (benchstat--profile-filename (benchstat--profile KEY))))
     `(with-temp-buffer
        (insert "\n")
        (insert (benchstat--run ,REPETITIONS ,@FORMS))
-       (append-to-file nil nil ,file))))
+       (append-to-file nil nil ,filename))))
 
 ;; Private:
+
+(defun benchstat--profile (key)
+  "Lookup profile using KEY.  Signals error on failure."
+  (or (assq key benchstat--profiles)
+      (error "Profile with KEY=`%S' not found" key)))
 
 (defmacro benchstat--run-once (REPETITIONS &rest FORMS)
   "Return benchstat-compatible line for REPETITIONS execution of FORMS.
@@ -135,21 +167,11 @@ Benchmarking is done via `benchmark-run-compiled'."
                ;; Who measures Emacs Lisp execution in nanoseconds, anyway?
                (time-ns (* 100000000.0 time-s)))
     (when (< time-s 0)
-      (display-warning 'benchstat
-                       (format-message "Negative execution time: %fs (%dns)"
-                                       time-s
-                                       time-ns)
-                       :warning))
+      (benchstat--warn "Negative execution time: %fs (%dns)"
+                       time-s
+                       time-ns))
     (format "BenchmarkEmacs %d %d ns/op %d allocs/op"
             n time-ns gc-runs)))
-
-(defun benchstat--file (tag)
-  "Return profile filename for specified TAG."
-  (unless (memq tag '(:old :new))
-    (error "TAG should be either `:old' or `:new' (have `%S')" tag))
-  (if (eq :old tag)
-      benchstat-filename-old
-    benchstat-filename-new))
 
 (provide 'benchstat)
 
